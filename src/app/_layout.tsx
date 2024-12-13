7e30bb89dc33e4b8cd024aab6dc77e66d48cf383
Pedro Marchioretto <pepepaiva221@gmail.com>
2024-12-13 17:11:24 -0300

    Home page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack } from "expo-router";
 import { colors } from "@/styles/theme"
 import { Rubik_600SemiBold, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold, useFonts } from "@expo-google-fonts/rubik"
 import { Loading } from "@/components/loading"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
 export default function Layout(){
   const [fontsLoaded] = useFonts({Rubik_600SemiBold, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold})
@@ -12,9 +13,14 @@ export default function Layout(){
     )
   }
 
-  return <Stack 
-    screenOptions={{ 
-      headerShown: false,
-      contentStyle: { backgroundColor: colors.gray[100] }
-    }}/>
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Stack 
+        screenOptions={{ 
+          headerShown: false,
+          contentStyle: { backgroundColor: colors.gray[100] }
+        }}
+      />
+    </GestureHandlerRootView>
+  )
 }
---
+++ src/components/category/index.tsx
@@ -0,0 +1,20 @@
+import { categoriesIcons } from "@/utils/categories-icons";
+import { s } from "./style";
+import { Text, Pressable, PressableProps} from "react-native";
+import { colors } from "@/styles/theme";
+
+type Props = PressableProps & {
+  iconId: string,
+  isSelected?: boolean,
+  name: string
+}
+
+export function Category ({ name, iconId, isSelected = false, ...rest } : Props){
+  const Icon = categoriesIcons[iconId]
+  return(
+    <Pressable style={[s.container, isSelected && s.containerSelected]} {...rest}>
+      <Icon size={20} color={colors.gray[isSelected ? 100 : 400]} />
+      <Text style={[s.name, isSelected && s.nameSelected]}>{name}</Text>
+    </Pressable>
+  )
+}
