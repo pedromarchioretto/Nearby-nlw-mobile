8d35bfd776b2465b2fea0a859c6b242cf18aa9c5
Pedro Marchioretto <pepepaiva221@gmail.com>
2024-12-15 23:03:46 -0300

    market screen and qr code scanning

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -4,15 +4,35 @@ import { useEffect, useState } from "react";
 import { Categories, CategoriesProps } from "@/components/categories";
 import { Places } from "@/components/places";
 import { PlaceProps } from "@/components/place";
+import MapView, { Marker, Callout } from "react-native-maps"
+import * as Location from 'expo-location';
+import { colors, fontFamily } from "@/styles/theme";
+import { router } from "expo-router" 
 
-type MarketsProps = PlaceProps
+type MarketsProps = PlaceProps & {
+  latitude: number,
+  longitude: number
+}
 
-export default function Home(){
+type LocationProps = {
+  latitude: number,
+  longitude: number,
+  latitudeDelta: number,
+  longitudeDelta: number
+}
+
+export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([])
   const [category, setCategory] = useState("")
   const [markets, setMarkets] = useState<MarketsProps[]>([])
+  const [currentLocation, setCurrentLocation] = useState<LocationProps>({
+    latitude: -23.561187293883442,
+    longitude: -46.656451388116494,
+    latitudeDelta: 0.1,
+    longitudeDelta: 0.1
+  }) // fake/random initial user location
 
-  async function fetchCategories(){ 
+  async function fetchCategories() {
     try {
       const { data } = await api.get("/categories")
       setCategories(data)
@@ -23,9 +43,9 @@ export default function Home(){
     }
   }
 
-  async function fetchMarkets(){
+  async function fetchMarkets() {
     try {
-      if(!category){
+      if (!category) {
         return
       }
 
@@ -37,18 +57,66 @@ export default function Home(){
     }
   }
 
+  async function getCurrentLocation() {
+    try {
+      const locationPermision = await Location.getForegroundPermissionsAsync()
+
+      const { status } = locationPermision.status == "undetermined" ?
+        await Location.requestForegroundPermissionsAsync() :
+        locationPermision.status == "granted" ? { "status": true } :
+          { "status": false }
+
+      if (status) {
+        const { coords } = await Location.getCurrentPositionAsync()
+        setCurrentLocation({ latitude: coords.latitude, longitude: coords.longitude, latitudeDelta: 0.01, longitudeDelta: 0.01 }) // current user location
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   useEffect(() => {
     fetchCategories()
+    getCurrentLocation()
+
   }, [])
 
   useEffect(() => {
     fetchMarkets()
   }, [category])
-  
-  return(
-    <View style={{flex: 1, backgroundColor: "#CECECE"}}>
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
       <Categories data={categories} onSelect={setCategory} selected={category} />
-      <Places data={markets}/>
+      <MapView
+        style={{ flex: 1 }}
+        region={currentLocation}
+      >
+        <Marker coordinate={currentLocation} identifier="current" image={require("@/assets/location.png")} />
+
+        {
+          markets.map((item) => (
+            <Marker key={item.id} identifier={item.id} coordinate={{ latitude: item.latitude, longitude: item.longitude }} image={require("@/assets/pin.png")}>
+
+              <Callout onPress={
+                // @ts-ignore
+                () => router.navigate(`/market/${item.id}`)}>
+                <View>
+                  <Text style={{ fontSize: 14, color: colors.gray[600], fontFamily: fontFamily.medium }}>
+                    {item.name}
+                  </Text>
+
+                  <Text style={{fontSize: 12, color: colors.gray[600], fontFamily: fontFamily.regular }}>
+                    {item.address}
+                  </Text>
+                </View>
+              </Callout>
+            </Marker>
+          ))
+        }
+      </MapView>
+      <Places data={markets} />
     </View>
   )
 }
--- src/components/places/index.tsx
+++ src/components/places/index.tsx
@@ -3,6 +3,7 @@ import { Place, PlaceProps } from "../place";
 import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet"
 import { useRef } from "react";
 import { s } from "./style";
+import { router } from "expo-router";
 
 type Props = {
   data: PlaceProps[]
@@ -12,7 +13,7 @@ export function Places({ data }: Props) {
   const dimensions = useWindowDimensions()
   const bottomSheetRef = useRef<BottomSheet>(null)
   const snapPoint = {
-    min: 278,
+    min: 140,
     max: dimensions.height - 128
   }
 
@@ -20,20 +21,27 @@ export function Places({ data }: Props) {
     <BottomSheet
       ref={bottomSheetRef}
       snapPoints={[snapPoint.min, snapPoint.max]}
-      handleIndicatorStyle = {s.indicator}
-      backgroundStyle = {s.container}
-      enableOverDrag = {false}
-      
+      handleIndicatorStyle={s.indicator}
+      backgroundStyle={s.container}
+      enableOverDrag={false}
+
     >
-      <BottomSheetFlatList 
+      <BottomSheetFlatList
         data={data}
         keyExtractor={(item) => item.id}
-        renderItem={({item}) => <Place data={item} />}
-        contentContainerStyle = {s.content}
+        renderItem={({ item }) => (
+          <Place
+            data={item}
+            
+            // @ts-ignore
+            onPress={() => router.navigate(`/market/${item.id}`)}
+          />
+        )}
+        contentContainerStyle={s.content}
         ListHeaderComponent={() => (
           <Text style={s.title}>Explore locais perto de você</Text>
         )}
-        showsVerticalScrollIndicator = {false}
+        showsVerticalScrollIndicator={false}
       />
     </BottomSheet>
   )
